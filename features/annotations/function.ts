2469e7ae8fd374dc13ab39ab946f08a28324804e
Sumna Islam <sumanislam1416@gmail.com>
2021-12-10 16:43:35 +0600

    learn how to annotate annonymous and name function

--- features/annotations/function.ts
+++ features/annotations/function.ts
@@ -3,5 +3,15 @@ const add = (a: number, b:number): number => {
 }
 
 const substract = (a: number, b:number): number => {
-  return a + b;
+  return a - b;
+}
+
+// function declaration
+function multiply(a: number, b: number): number {
+  return a * b;
+}
+
+// anonymous function
+const divide = function(a: number, b: number): number {
+  return a / b;
 }
