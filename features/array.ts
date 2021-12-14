a692f736c08b286b2576cf38b74db0f758aad598
Sumna Islam <sumanislam1416@gmail.com>
2021-12-14 09:13:34 +0600

    learn array in typeScript

---
+++ features/array.ts
@@ -0,0 +1,11 @@
+const carMakers1 = ['Ford', 'Ferrari', 'Chevy', 'Toyota'];
+const carMakers2: string[] = ['Ford', 'Ferrari', 'Chevy', 'Toyota'];
+const carMakers3: string[] = [];
+const dates = [new Date(), new Date()];
+
+const carsByMake = [
+  ['f150'],
+  ['corolla'],
+  ['camaro']
+];
+const carsByMake2: string[][] = [];
