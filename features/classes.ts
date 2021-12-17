5f840207569ffd33746d122503e7ba5c14fa7930
Sumna Islam <sumanislam1416@gmail.com>
2021-12-17 15:14:30 +0600

    instance method modifiers

--- features/classes.ts
+++ features/classes.ts
@@ -1,19 +1,21 @@
 class Vehicle {
-  drive(): void {
-    console.log('chugga chugga');
-  }
-  hunk(): void {
+  protected honk(): void {
     console.log('beep');
   }
 }
 
 class Car extends Vehicle {
-  drive(): void {
+  private drive(): void {
       console.log('vroom');
+  }
+
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
   }
 }
 
 const car = new Car();
 
-car.drive();
-car.hunk();
+car.startDrivingProcess();
+car.honk();
