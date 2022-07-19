0b5b2f096f6c77d90517efa8a466ee939ea59362
Suman Islam <sumanislam1416@gmail.com>
2022-07-19 12:54:45 +0600

    add some change again

--- features/interfaces.ts
+++ features/interfaces.ts
@@ -1,35 +1,45 @@
 interface Vehicle {
   name: string;
-  year: Date;
+  year: Number;
   broken: boolean;
-  summary(): string;
 }
 
 const oldCivic = {
-  name: 'civic',
-  year: new Date(),
-  broken: true,
-  summary(): string {
-    return `Name: ${this.name}`
-  }
+	name: 'civic',
+	year: 2000,
+	broken: true
 };
+
+// const printVehicle = (vehicle: {
+// 	name: string;
+// 	year: Number;
+// 	isBroken: Boolean;
+// }): void => {
+// 	console.log(`
+//     Name: ${vehicle.name}
+//     Year: ${vehicle.year}
+//     Broken? ${vehicle.isBroken}
+//   `);
+// };
+
+// printVehicle(oldCivic);
+
+const printVehicle = (vehicle: Vehicle): void => {
+	console.log(`
+    Name: ${vehicle.name}
+    Year: ${vehicle.year}
+    Broken? ${vehicle.broken}
+  `);
+};
+
+printVehicle(oldCivic);
 
 // will generate error because broken property is not available
 const oldCivic2 = {
-  name: 'civic',
-  year: 2000,
-  isBroken: true,  
+	name: 'civic',
+	year: new Date(),
+	broken: true,
+	summary(): string {
+		return `Name: ${this.name}`;
+	},
 };
-
-const printVehicle = (vehicle: { name: string; year: Number; isBroken: Boolean }): void => {
-  // console.log(vehicle.summary());
-  console.log(`
-    Name: ${vehicle.name}
-    Year: ${vehicle.year}
-    Broken? ${vehicle.isBroken}
-  `);
-  
-  
-}
-
-printVehicle(oldCivic2);
