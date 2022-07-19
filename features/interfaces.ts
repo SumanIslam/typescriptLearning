900bb17cfea4a15eb864dab21352682bf02a5153
Suman Islam <sumanislam1416@gmail.com>
2022-07-19 14:23:19 +0600

    add some change again

--- features/interfaces.ts
+++ features/interfaces.ts
@@ -24,7 +24,29 @@ const oldCivic = {
 
 // printVehicle(oldCivic);
 
-const printVehicle = (vehicle: Vehicle): void => {
+// const printVehicle = (vehicle: Vehicle): void => {
+// 	console.log(`
+//     Name: ${vehicle.name}
+//     Year: ${vehicle.year}
+//     Broken? ${vehicle.broken}
+//   `);
+// };
+
+// printVehicle(oldCivic);
+
+interface Vehicle2 {
+	name: string;
+	year: Date;
+	broken: boolean;
+}
+
+const oldCivic2 = {
+	name: 'civic',
+	year: new Date(),
+	broken: true,
+};
+
+const printVehicle = (vehicle: Vehicle2): void => {
 	console.log(`
     Name: ${vehicle.name}
     Year: ${vehicle.year}
@@ -32,10 +54,11 @@ const printVehicle = (vehicle: Vehicle): void => {
   `);
 };
 
-printVehicle(oldCivic);
+printVehicle(oldCivic2);
+
 
 // will generate error because broken property is not available
-const oldCivic2 = {
+const oldCivic3 = {
 	name: 'civic',
 	year: new Date(),
 	broken: true,
