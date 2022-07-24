417fbb6df964d95e662a7f558562b43d4b1bf520
Suman Islam <sumanislam1416@gmail.com>
2022-07-24 14:34:31 +0600

    updating todo completed status in local storage

--- ts-todo/dist/index.js
+++ ts-todo/dist/index.js
@@ -3,34 +3,54 @@ const btn = document.getElementById('btn');
 const input = document.getElementById('todoinput');
 const form = document.querySelector('form');
 const list = document.getElementById('todolist');
+// Todos array
 const todos = readTodos();
+// showing todos to the dom
 todos.forEach(todo => {
     createTodo(todo);
 });
+// function to save todos in local storage
+function saveTodos() {
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+// function that read all the todos from local storage
 function readTodos() {
     const todosJSON = localStorage.getItem('todos');
     if (todosJSON === null)
         return [];
     return JSON.parse(todosJSON);
 }
+// function that create an individual todo
 function createTodo(todo) {
     const newLI = document.createElement('li');
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
-    checkbox.checked = false;
+    checkbox.checked = todo.completed;
+    // event listener for updating completed status of a todo
+    checkbox.addEventListener('change', () => {
+        todo.completed = checkbox.checked;
+        // saveTodos function calls
+        saveTodos();
+    });
     newLI.append(todo.text);
     newLI.append(checkbox);
     list.append(newLI);
 }
+// handle form submission
 function handleSubmit(e) {
     e.preventDefault();
     const newTodo = {
         text: input.value,
         completed: false,
     };
+    // createTodo function calls
     createTodo(newTodo);
+    // push todo in todos array
     todos.push(newTodo);
-    localStorage.setItem('todos', JSON.stringify(todos));
+    // saveTodos function calls
+    saveTodos();
+    // set input value to "" (empty string)
     input.value = '';
 }
+// event listener for form submission
 form.addEventListener('submit', handleSubmit);
--- ts-todo/src/index.ts
+++ ts-todo/src/index.ts
@@ -6,32 +6,56 @@ const form = document.querySelector('form')!;
 
 const list = document.getElementById('todolist')!;
 
+// interface for a Todo
 interface Todo {
 	text: string;
 	completed: boolean;
 }
 
+// Todos array
 const todos: Todo[] = readTodos();
+
+// showing todos to the dom
 todos.forEach(todo => {
 	createTodo(todo);
-})
+});
 
+// function to save todos in local storage
+function saveTodos(): void {
+	localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+// function that read all the todos from local storage
 function readTodos(): Todo[] {
 	const todosJSON = localStorage.getItem('todos');
 	if(todosJSON === null)  return [];
 	return JSON.parse(todosJSON);
 }
 
+
+// function that create an individual todo
 function createTodo(todo: Todo): void {
 	const newLI = document.createElement('li');
+
 	const checkbox = document.createElement('input');
 	checkbox.type = 'checkbox';
-	checkbox.checked = false;
+	checkbox.checked = todo.completed;
+
+	// event listener for updating completed status of a todo
+	checkbox.addEventListener('change', () => {
+		todo.completed = checkbox.checked;
+		
+		// saveTodos function calls
+		saveTodos();
+	})
+
 	newLI.append(todo.text);
 	newLI.append(checkbox);
 	list.append(newLI);
 }
 
+
+// handle form submission
 function handleSubmit(e: SubmitEvent): void {
 	e.preventDefault();
 
@@ -40,12 +64,17 @@ function handleSubmit(e: SubmitEvent): void {
 		completed: false,
 	};
 
+	// createTodo function calls
 	createTodo(newTodo);
+
+	// push todo in todos array
 	todos.push(newTodo);
 
-	localStorage.setItem('todos', JSON.stringify(todos));
-
+	// saveTodos function calls
+	saveTodos();
+	// set input value to "" (empty string)
 	input.value = '';
 }
 
+// event listener for form submission
 form.addEventListener('submit', handleSubmit);
