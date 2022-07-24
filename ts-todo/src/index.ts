7af5cd120fd9ee4da6888ba8c8e3315ccf2de14b
Suman Islam <sumanislam1416@gmail.com>
2022-07-24 13:36:29 +0600

    working in  project

--- ts-todo/src/index.ts
+++ ts-todo/src/index.ts
@@ -1,1 +1,7 @@
-console.log('Hello!!');
+const btn = document.getElementById('btn')! as HTMLButtonElement;
+const input = document.getElementById('todoinput')! as HTMLInputElement;
+
+btn.addEventListener('click', function () {
+	alert(input.value);
+	input.value = '';
+});
