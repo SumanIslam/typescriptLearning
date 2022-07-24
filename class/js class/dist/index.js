86d2a07dcd151149b9a003c8b18fa0d3b881a856
Suman Islam <sumanislam1416@gmail.com>
2022-07-24 18:40:38 +0600

    working with constructor

--- class/js class/dist/index.js
+++ class/js class/dist/index.js
@@ -1,12 +1,20 @@
 class Player {
+  constructor(first, last) {
+    this.first = first;
+    this.last = last;
+  }
   taunt() {
     console.log("BooYah");
   }
 }
 
 
-const player1 = new Player();
+const player1 = new Player("Suman", "Islam");
 player1.taunt();
+console.log(player1.first);
+console.log(player1.last);
 
-const player2 = new Player();
+const player2 = new Player("Charlie", "brown");
 player2.taunt();
+console.log(player2.first);
+console.log(player2.last);
