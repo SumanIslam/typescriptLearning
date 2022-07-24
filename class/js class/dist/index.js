2a3674f5cba73173042e96652d47a51f01253ac0
Suman Islam <sumanislam1416@gmail.com>
2022-07-24 18:35:36 +0600

    working with classes

--- class/js class/dist/index.js
+++ class/js class/dist/index.js
@@ -1,2 +1,12 @@
-"use strict";
-console.log('Its working!');
+class Player {
+  taunt() {
+    console.log("BooYah");
+  }
+}
+
+
+const player1 = new Player();
+player1.taunt();
+
+const player2 = new Player();
+player2.taunt();
