bb9974d43fef1c22def9a96b2fe81fc1647e0cdb
Suman Islam <sumanislam1416@gmail.com>
2022-07-26 18:08:44 +0600

    working with class fields

--- class/js class/dist/index.js
+++ class/js class/dist/index.js
@@ -1,4 +1,6 @@
 class Player {
+  score = 0;
+  numLives = 10;
   constructor(first, last) {
     this.first = first;
     this.last = last;
@@ -6,15 +8,22 @@ class Player {
   taunt() {
     console.log("BooYah");
   }
+  loseLife() {
+    this.numLives -= 1;
+  }
 }
 
 
 const player1 = new Player("Suman", "Islam");
-player1.taunt();
-console.log(player1.first);
-console.log(player1.last);
+// player1.taunt();
+// console.log(player1.first);
+// console.log(player1.last);
+// console.log(player1);
+console.log(player1.numLives);
+player1.loseLife();
+console.log(player1.numLives);
 
-const player2 = new Player("Charlie", "brown");
-player2.taunt();
-console.log(player2.first);
-console.log(player2.last);
+// const player2 = new Player("Charlie", "brown");
+// player2.taunt();
+// console.log(player2.first);
+// console.log(player2.last);
