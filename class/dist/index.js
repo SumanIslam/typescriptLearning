e42f6c3993392500d35c7160e88e4aaf91843be0
Suman Islam <sumanislam1416@gmail.com>
2022-07-26 23:03:52 +0600

    annotating classes in ts

--- class/dist/index.js
+++ class/dist/index.js
@@ -1,64 +1,10 @@
-class Player {
-  static description = 'Player in our game';
-  #score = 0;
-  numLives = 10;
-  constructor(first, last) {
-    this.first = first;
-    this.last = last;
-  }
-  static randomPlayer() {
-    return new Player("Andy", "Samberg")
-  }
-  get fullName() {
-    return `${this.first} ${this.last}`
-  }
-  getScore() {
-    return this.#score;
-  }
-  set score(newScore) {
-    if(newScore < 0) {
-      throw new Error("Score Must be positive");
+"use strict";
+var Player = /** @class */ (function () {
+    function Player(first, last) {
+        this.first = first;
+        this.last = last;
     }
-    this.#score = newScore;
-  }
-  updateScore(newScore) {
-    return this.#score = newScore;
-  }
-  taunt() {
-    console.log("BooYah");
-  }
-  loseLife() {
-    this.numLives -= 1;
-  }
-}
-
-class AdminPlayer extends Player {
-  isAdmin = true;
-  constructor(powers, first, last) {
-    super(first, last);
-    this.powers = powers;
-  }
-}
-
-const admin = new AdminPlayer(["delete", "restore world"], "admin", "mcadian");
-console.log(admin);
-
-const player1 = new Player("Suman", "Islam");
-// player1.taunt();
-// console.log(player1.first);
-// console.log(player1.last);
-// console.log(player1);
-// console.log(Player.description);
-// console.log(Player.randomPlayer());
-// console.log(player1.numLives);
-// player1.loseLife();
-// console.log(player1.numLives);
-// console.log(player1.getScore());
-// player1.score = 23;
-// console.log(player1.getScore());
-// console.log(player1.fullName);
-
-// const player2 = new Player("Charlie", "brown");
-// player2.taunt();
-// console.log(player2.first);
-// console.log(player2.last);
+    return Player;
+}());
+var elton = new Player("Elton", "Steele");
+console.log(elton);
