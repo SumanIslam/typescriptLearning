fd0103d778d892ef4641d2e65fc10a614ee4200f
Suman Islam <sumanislam1416@gmail.com>
2022-07-26 23:23:11 +0600

    getters and setters in ts

--- class/dist/index.js
+++ class/dist/index.js
@@ -3,10 +3,33 @@ var Player = /** @class */ (function () {
     function Player(first, last) {
         this.first = first;
         this.last = last;
-        this.score = 0;
+        this._score = 0;
     }
+    Object.defineProperty(Player.prototype, "fullName", {
+        get: function () {
+            return "".concat(this.first, " ").concat(this.last);
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Player.prototype, "score", {
+        get: function () {
+            return this._score;
+        },
+        set: function (newScore) {
+            if (newScore < 0) {
+                throw new Error('Score can not be negative');
+            }
+            this._score = newScore;
+        },
+        enumerable: false,
+        configurable: true
+    });
     return Player;
 }());
 var elton = new Player("Elton", "Steele");
 console.log(elton);
-// elton.score
+console.log(elton.fullName);
+console.log(elton.score);
+elton.score = 23;
+console.log(elton.score);
--- class/src/index.ts
+++ class/src/index.ts
@@ -1,10 +1,28 @@
 class Player {
-  private score: number = 0;
+  private _score: number = 0;
 
   constructor(public first: string, public last: string) {}
+
+  get fullName(): string {
+    return `${this.first} ${this.last}`
+  }
+
+  get score(): number {
+    return this._score;
+  }
+
+  set score(newScore: number) {
+    if(newScore < 0) {
+      throw new Error('Score can not be negative')
+    }
+    this._score = newScore;
+  }
 }
 
 const elton = new Player("Elton", "Steele");
 console.log(elton);
 
-// elton.score
+console.log(elton.fullName);
+console.log(elton.score);
+elton.score  = 23
+console.log(elton.score);
