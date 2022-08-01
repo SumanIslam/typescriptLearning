9c3a5e2da2d5801f0ec995c32a050513616d1563
Suman Islam <sumanislam1416@gmail.com>
2022-08-01 14:37:27 +0600

    create shopping list component

--- tsReact/my-app/src/App.tsx
+++ tsReact/my-app/src/App.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
 import './App.css';
-import Greeter from './components/Greeter';
+import ShoppingList from './components/ShoppingList';
 
 function App() {
-  return (
-    <div className="App">
-      <Greeter person="Colt" />
-      <Greeter person="Steele" />
-    </div>
-  );
+	const items = [
+		{
+			id: 1,
+			product: 'Lemon',
+			quantity: 3,
+		},
+		{
+			id: 2,
+			product: 'Chicken Breast',
+			quantity: 2,
+		},
+	];
+	return (
+		<div>
+			<ShoppingList items = {items}/>
+		</div>
+	);
 }
 
 export default App;
